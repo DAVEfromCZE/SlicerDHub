e9a9e26a70b26895204c242b6472709042117b3b
HDaveSoft <38252011+DAVEfromCZE@users.noreply.github.com>
2025-06-20 08:37:13 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --noconfirm --onefile --windowed --icon=SlicerDHub.ico --hidden-import=win32gui --hidden-import=win32api --hidden-import=win32con --hidden-import=win32ui slicerdhub.py
+pyinstaller --noconfirm --onefile --windowed --icon=SlicerDHub.ico --hidden-import=win32gui --hidden-import=win32api --hidden-import=win32con --hidden-import=win32ui SlicerDHub.py
 pause
